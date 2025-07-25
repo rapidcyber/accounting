24ffb269a5c8b82cef12ed59a05ff48f396bad2b
rapidcyber <jun.barcellano@realholmes.ph>
2025-07-25 16:42:17 +0800

    Change main max width to full

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -69,6 +69,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->maxContentWidth("1440px");
     }
 }
